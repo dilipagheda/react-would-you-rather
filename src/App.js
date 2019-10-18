d6fd071a5208b53d815c079a7f21b2614c0b28d0
Dilip <dilip_agheda@yahoo.com>
2019-10-19 10:31:33 +1100

    initial version of App.js for basic testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import { Container } from 'react-bootstrap';
+
+import AppMenu from './components/AppMenu';
+import HomeView from './components/home/HomeView';
+
 function App() {
-	return <div>App</div>;
+	return (
+		<div>
+			<AppMenu />
+			<Container>
+				<div className="d-flex flex-row-reverse">
+					<span style={{ padding: '0 30px', backgroundColor: 'grey', color: 'white', borderRadius: '30px' }}>
+						Hello user!
+					</span>
+				</div>
+				<HomeView />
+			</Container>
+		</div>
+	);
 }
 
 export default App;
