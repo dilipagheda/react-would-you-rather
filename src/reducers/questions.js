e4bd0f6dfa9194e63a753231904450797efd6426
Dilip <dilip_agheda@yahoo.com>
2019-10-19 10:31:08 +1100

    initial setup for reducers

---
+++ src/reducers/questions.js
@@ -0,0 +1,6 @@
+// import { RECEIVE_TWEETS } from '../actions/tweets'
+import { Questions } from '../utils/_DATA';
+
+export default function questions(state = Questions, action) {
+	return state;
+}
---
+++ src/reducers/users.js
@@ -0,0 +1,6 @@
+// import { RECEIVE_TWEETS } from '../actions/tweets'
+import { Users } from '../utils/_DATA';
+
+export default function users(state = Users, action) {
+	return state;
+}
