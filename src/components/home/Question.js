3e0c27d56f3ff5e7fa741fa6dc6c4050be88d92f
Dilip <dilip_agheda@yahoo.com>
2019-10-19 12:14:44 +1100

    Wired up Question to the store and wrote logic to get props

--- src/components/home/Question.js
+++ src/components/home/Question.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import { connect } from 'react-redux';
 
 class Question extends Component {
 	render() {
 		return (
-			<div style={{ width: '50%', margin: 'auto', backgroundColor: 'lightgrey' }}>
+			<div
+				style={{
+					width: '50%',
+					margin: '10px auto',
+					backgroundColor: 'lightgrey',
+					borderRadius: '10px'
+				}}
+			>
 				<div
 					style={{
 						fontWeight: 'bold',
 						fontSize: '20px',
 						backgroundColor: 'grey',
 						padding: '10px',
-						color: 'white'
+						color: 'white',
+						borderRadius: '10px 10px 0 0'
 					}}
 				>
-					Sarah asks:
+					{this.props.authorName} asks:
 				</div>
 				<div style={{ display: 'flex', padding: '10px' }}>
-					<img
-						src="https://i.pravatar.cc/150?img=3"
-						style={{ width: '100px', height: '100px', borderRadius: '50%' }}
-					/>
+					<img src={this.props.avatarURL} style={{ width: '100px', height: '100px', borderRadius: '50%' }} />
 					<div style={{ marginLeft: '10px' }}>
-						<div style={{ fontWeight: 'bold', fontSize: '20px' }}>Would you rather?</div>
-						<div style={{ margin: '10px 0' }}>..be a frontend...</div>
+						<div style={{ fontWeight: 'bold', fontSize: '20px' }}>Would you rather</div>
+						<div style={{ margin: '10px 0' }}>{this.props.optionOne}?</div>
+						<div>OR</div>
+						<div style={{ margin: '10px 0' }}>{this.props.optionTwo}?</div>
+
 						<Button>View Poll</Button>
 					</div>
 				</div>
@@ -32,4 +41,22 @@ class Question extends Component {
 	}
 }
 
-export default Question;
+function mapStateToProps({ users, questions }, { id }) {
+	const question = questions[id];
+	const author = users[question.author];
+	// const words = question.optionOne.text.split(' ');
+	// let shortOption = '';
+	// if (words && words.length > 2) {
+	// 	shortOption = `...${words[1]}...`;
+	// } else if (words && words.length > 1) {
+	// 	shortOption = `...${words[0]}...`;
+	// }
+	// shortOption = question.optionOne.text;
+	return {
+		authorName: author.name,
+		avatarURL: author.avatarURL,
+		optionOne: question.optionOne.text,
+		optionTwo: question.optionTwo.text
+	};
+}
+export default connect(mapStateToProps)(Question);
