c4f8af34b63bc39fcbef333210bf84b00e2a2339
Dilip <dilip_agheda@yahoo.com>
2019-10-19 22:43:11 +1100

    styling of AppMenu

--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -4,18 +4,26 @@ import { Nav, Navbar, Container } from 'react-bootstrap';
 class AppMenu extends Component {
 	render() {
 		return (
-			<Navbar bg="light" expand="lg">
+			<Navbar expand="lg" style={{ backgroundColor: '#46344e' }}>
 				<Container>
-					<Navbar.Brand href="#home">Would you rather?</Navbar.Brand>
+					<Navbar.Brand href="#home" style={{ color: 'white' }}>
+						Would you rather?
+					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="mr-auto">
-							<Nav.Link href="#home">Home</Nav.Link>
-							<Nav.Link href="#link">New Question</Nav.Link>
-							<Nav.Link href="#link">Leaderboard</Nav.Link>
+							<Nav.Link href="#home" style={{ color: 'white' }}>
+								Home
+							</Nav.Link>
+							<Nav.Link href="#link" style={{ color: 'white' }}>
+								New Question
+							</Nav.Link>
+							<Nav.Link href="#link" style={{ color: 'white' }}>
+								Leaderboard
+							</Nav.Link>
 						</Nav>
 						<Nav>
-							<Nav.Link href="#link" className="justify-content-end">
+							<Nav.Link href="#link" className="justify-content-end" style={{ color: 'white' }}>
 								Logout
 							</Nav.Link>
 						</Nav>
