9b3c2bfd86055c8b50b93cad6a3b517015c2ce6e
Dilip <dilip_agheda@yahoo.com>
2019-10-19 12:15:11 +1100

    Wired up UnansweredQuestions to the store and wrote logic to get props

--- src/components/home/UnAnsweredQuestions.js
+++ src/components/home/UnAnsweredQuestions.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuestionList from './QuestionList';
+import { connect } from 'react-redux';
 
 class UnansweredQuestions extends Component {
 	render() {
@@ -8,10 +9,10 @@ class UnansweredQuestions extends Component {
 }
 
 function mapStateToProps({ users, questions }, { userId }) {
-	const user = users[id];
+	const user = users[userId];
 	const answers = user.answers;
 	const answeredQuestionIds = Object.keys(answers);
-	const unansweredQuestionIds = Object.keys(questions).filter((id) => answeredQuestionIds.includes(id));
+	const unansweredQuestionIds = Object.keys(questions).filter((id) => !answeredQuestionIds.includes(id));
 
 	return {
 		unansweredQuestionIds
