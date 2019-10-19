d6adb88ec9169b45ae9b8fc81ccc4b0b0a5ad9a1
Dilip <dilip_agheda@yahoo.com>
2019-10-19 22:43:44 +1100

    included custom styles

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 
-// import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 // const store = createStore(reducer);
