92859c38fd8c004949b2d534f9f2a1e3c742484f
Dilip <dilip_agheda@yahoo.com>
2019-10-21 00:59:28 +1100

    Fixed AppMenu for current selection

--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { Nav, Navbar, Container } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 class AppMenu extends Component {
+	state = {
+		currentSelection: 'home'
+	};
+	handleSelect = (e) => {
+		this.setState({ currentSelection: e });
+	};
 	render() {
 		return (
 			<Navbar expand="lg" style={{ backgroundColor: '#46344e' }}>
@@ -11,14 +18,43 @@ class AppMenu extends Component {
 					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" style={{ backgroundColor: 'white' }} />
 					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="mr-auto">
-							<Nav.Link href="/" style={{ color: 'white' }}>
+						<Nav className="mr-auto" onSelect={this.handleSelect}>
+							{/* <NavLink exact activeClassName="active" to="/">
+								Home
+							</NavLink>
+							<NavLink activeClassName="active" to="/add">
+								New Question
+							</NavLink>
+							<NavLink activeClassName="active" to="/leaderboard">
+								Leaderboard
+							</NavLink> */}
+
+							<Nav.Link
+								active={this.state.currentSelection === 'home'}
+								eventKey="home"
+								as={NavLink}
+								exact
+								to="/"
+								style={{ color: 'white' }}
+							>
 								Home
 							</Nav.Link>
-							<Nav.Link href="/add" style={{ color: 'white' }}>
+							<Nav.Link
+								active={this.state.currentSelection === 'newquestion'}
+								eventKey="newquestion"
+								as={NavLink}
+								to="/add"
+								style={{ color: 'white' }}
+							>
 								New Question
 							</Nav.Link>
-							<Nav.Link href="/leaderboard" style={{ color: 'white' }}>
+							<Nav.Link
+								active={this.state.currentSelection === 'leaderboard'}
+								eventKey="leaderboard"
+								as={NavLink}
+								to="/leaderboard"
+								style={{ color: 'white' }}
+							>
 								Leaderboard
 							</Nav.Link>
 						</Nav>
