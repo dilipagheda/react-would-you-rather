bfe573d073c9ab3fd0d9108cc3adc0dbc3fc6425
Dilip <dilip_agheda@yahoo.com>
2019-10-20 22:05:51 +1100

    Updated App.js to include Loader

--- src/App.js
+++ src/App.js
@@ -1,49 +1,67 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { Container } from 'react-bootstrap';
-
 import AppMenu from './components/AppMenu';
 import HomeView from './components/home/HomeView';
-import PollResultView from './components/poll/PollResultView';
 import LeaderBoardView from './components/leaderboard/LeaderBoardView';
 import PollMain from './components/poll/PollMain';
 import CreateQuestion from './components/question/CreateQuestion';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import NotFound from './components/shared/NotFound';
-function App() {
-	return (
-		<div>
-			<AppMenu />
-			<Container>
-				<div className="d-flex flex-row-reverse">
-					<span
-						style={{
-							padding: '0 30px',
-							backgroundColor: '#FCCD04',
-							color: '#A64AC9',
-							borderRadius: '0 0 10px 10px'
-						}}
-					>
-						Hello user!
-					</span>
-				</div>
-				{/* <HomeView /> */}
-				{/* <PollResultView /> */}
-				{/* <PollForm /> */}
-				{/* <LeaderBoardView /> */}
-				{/* <CreateQuestion /> */}
-				<Router>
-					<Switch>
-						<Route exact path="/" component={HomeView} />
-						<Route path="/add" component={CreateQuestion} />
-						<Route path="/leaderboard" component={LeaderBoardView} />
-						<Route path="/questions/:question_id" component={PollMain} />
-						<Route component={NotFound} />
-					</Switch>
-				</Router>
-			</Container>
-		</div>
-	);
-}
+import Loader from './components/shared/Loader';
+import { handleInitialData } from './actions/shared';
 
-export default App;
+class App extends Component {
+	componentDidMount() {
+		this.props.dispatch(handleInitialData());
+	}
+
+	render() {
+		return (
+			<div>
+				<AppMenu />
+				{
+					this.props.loader ? <Container
+						className="d-flex justify-content-center"
+						style={{ height: '100vh' }}
+					>
+						<Loader />
+					</Container> :
+					<Container>
+						<div className="d-flex flex-row-reverse">
+							<span
+								style={{
+									padding: '0 30px',
+									backgroundColor: '#FCCD04',
+									color: '#A64AC9',
+									borderRadius: '0 0 10px 10px'
+								}}
+							>
+								Hello user!
+							</span>
+						</div>
+						{/* <HomeView /> */}
+						{/* <PollResultView /> */}
+						{/* <PollForm /> */}
+						{/* <LeaderBoardView /> */}
+						{/* <CreateQuestion /> */}
+						<Router>
+							<Switch>
+								<Route exact path="/" component={HomeView} />
+								<Route path="/add" component={CreateQuestion} />
+								<Route path="/leaderboard" component={LeaderBoardView} />
+								<Route path="/questions/:question_id" component={PollMain} />
+								<Route component={NotFound} />
+							</Switch>
+						</Router>
+					</Container>}
+			</div>
+		);
+	}
+}
+function mapStateToProps({ authedUser, loader }) {
+	return {
+		loader
+	};
+}
+export default connect(mapStateToProps)(App);
