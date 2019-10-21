ef21b900ad16330452146c9caa9b5105cc460153
Dilip <dilip_agheda@yahoo.com>
2019-10-21 20:34:01 +1100

    Implemented LeaderBoard and wired up to redux store

--- src/components/leaderboard/LeaderBoardView.js
+++ src/components/leaderboard/LeaderBoardView.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 import ScoreCard from './ScoreCard';
+import { connect } from 'react-redux';
 
 class LeaderBoardView extends Component {
+	renderUserScores = () => this.props.userScores.map((userScore) => <ScoreCard key={userScore.id} {...userScore} />);
+
 	render() {
-		return (
-			<Container>
-				<ScoreCard />
-			</Container>
-		);
+		return <Container>{this.renderUserScores()}</Container>;
 	}
 }
 
-export default LeaderBoardView;
+function mapStateToProps({ users }) {
+	const userScores = Object.values(users)
+		.map((user) => {
+			const totalAnswered = Object.keys(user.answers).length;
+			const totalQuestions = user.questions.length;
+			const score = totalAnswered + totalQuestions;
+			return {
+				id: user.id,
+				name: user.name,
+				avatarURL: user.avatarURL,
+				totalAnswered,
+				totalQuestions,
+				score
+			};
+		})
+		.sort((a, b) => b.score - a.score);
+
+	return { userScores };
+}
+
+export default connect(mapStateToProps)(LeaderBoardView);
--- src/components/leaderboard/ScoreCard.js
+++ src/components/leaderboard/ScoreCard.js
@@ -1,43 +1,48 @@
 import React, { Component } from 'react';
 import { OuterDiv, DetailDiv, TotalScore } from '../shared/Shared';
+import { Row, Col } from 'react-bootstrap';
 
 class ScoreCard extends Component {
 	render() {
 		return (
-			<OuterDiv>
-				<DetailDiv>
-					<img
-						src="https://i.pravatar.cc/150?img=3"
-						style={{ borderRadius: '50%', height: '100px', width: '100px', alignSelf: 'center' }}
-					/>
-					<div
-						style={{
-							margin: '0 10px',
-							borderLeft: '1px solid grey',
-							borderRight: '1px solid grey',
-							padding: '0 10px',
-							flexGrow: 1,
-							justifySelf: 'center'
-						}}
-					>
-						<div style={{ fontWeight: 'bold', fontSize: '20px' }}>Dilip Agheda</div>
-						<table>
-							<tr>
-								<td>Answered Questions</td>
-								<td>10</td>
-							</tr>
-							<tr>
-								<td>Created Questions</td>
-								<td>2</td>
-							</tr>
-						</table>
-					</div>
-					<div style={{ alignSelf: 'center' }}>
-						<div style={{ fontWeight: 'bold', fontSize: '20px' }}>Score</div>
-						<TotalScore>12</TotalScore>
-					</div>
-				</DetailDiv>
-			</OuterDiv>
+			<Row>
+				<Col sm={12} md={8} lg={6} style={{ margin: 'auto' }}>
+					<OuterDiv>
+						<DetailDiv>
+							<img
+								src={this.props.avatarURL}
+								style={{ borderRadius: '50%', height: '100px', width: '100px', alignSelf: 'center' }}
+							/>
+							<div
+								style={{
+									margin: '0 10px',
+									borderLeft: '1px solid grey',
+									borderRight: '1px solid grey',
+									padding: '0 10px',
+									flexGrow: 1,
+									justifySelf: 'center'
+								}}
+							>
+								<div style={{ fontWeight: 'bold', fontSize: '20px' }}>{this.props.name}</div>
+								<table>
+									<tr>
+										<td>Answered Questions</td>
+										<td>{this.props.totalAnswered}</td>
+									</tr>
+									<tr>
+										<td>Created Questions</td>
+										<td>{this.props.totalQuestions}</td>
+									</tr>
+								</table>
+							</div>
+							<div style={{ alignSelf: 'center' }}>
+								<div style={{ fontWeight: 'bold', fontSize: '20px' }}>Score</div>
+								<TotalScore>{this.props.score}</TotalScore>
+							</div>
+						</DetailDiv>
+					</OuterDiv>
+				</Col>
+			</Row>
 		);
 	}
 }
