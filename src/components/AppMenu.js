8d9d36120950cd17a730f58a4f070b2634f79732
Dilip <dilip_agheda@yahoo.com>
2019-10-22 23:18:19 +1100

    Fixed issues with AppMenu

--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -13,13 +13,13 @@ class AppMenu extends Component {
 		return (
 			<Navbar expand="lg" style={{ backgroundColor: '#46344e' }}>
 				<Container>
-					<Navbar.Brand href="#home" style={{ color: 'white' }}>
-						Would you rather?
-					</Navbar.Brand>
+					<NavLink exact to="/">
+						<Navbar.Brand style={{ color: 'white' }}>Would you rather?</Navbar.Brand>
+					</NavLink>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" style={{ backgroundColor: 'white' }} />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="mr-auto" onSelect={this.handleSelect} style={{ width: '100%' }}>
-							<NavLink exact activeClassName="active" to="/">
+							<NavLink activeClassName="active" to="/home">
 								Home
 							</NavLink>
 							<NavLink activeClassName="active" to="/add">
@@ -31,34 +31,6 @@ class AppMenu extends Component {
 							<NavLink to="/logout" style={{ color: 'white', marginLeft: 'auto' }}>
 								Logout
 							</NavLink>
-							{/* <Nav.Link
-								active={this.state.currentSelection === 'home'}
-								eventKey="home"
-								as={NavLink}
-								exact
-								to="/"
-								style={{ color: 'white' }}
-							>
-								Home
-							</Nav.Link>
-							<Nav.Link
-								active={this.state.currentSelection === 'newquestion'}
-								eventKey="newquestion"
-								as={NavLink}
-								to="/add"
-								style={{ color: 'white' }}
-							>
-								New Question
-							</Nav.Link>
-							<Nav.Link
-								active={this.state.currentSelection === 'leaderboard'}
-								eventKey="leaderboard"
-								as={NavLink}
-								to="/leaderboard"
-								style={{ color: 'white' }}
-							>
-								Leaderboard
-							</Nav.Link> */}
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
