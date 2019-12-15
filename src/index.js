a13e2e0ba9daa6a1c053672058a88120036dc683
Dilip <dilip_agheda@yahoo.com>
2019-12-15 18:18:44 +1100

    Added redux dev tools to support debugging

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
 	<Provider store={store}>
